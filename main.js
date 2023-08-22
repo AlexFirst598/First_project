5c2a97c35a83cf34d9fe4d84dda290c290f0f0dd
AlexFirst598 <141620329+AlexFirst598@users.noreply.github.com>
2023-08-22 11:25:24 +0300

    Файл

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// модальное окно
 const button = document.querySelector('#OpenGui');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close-img');
 const InputActive = document.querySelector('#InputFocus');
+
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -12,4 +14,27 @@ button.addEventListener('click', (e) => {
 close.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.remove('modal-active');
+});
+
+// слайдер
+
+const dots = document.querySelectorAll('.dot');
+const slides = document.querySelectorAll('.slide');
+
+// выключение всех слайдов
+function hideSlides() {
+    slides.forEach(slide => {
+        slide.classList.remove('slide-active');
+    });
+}
+
+dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+        hideSlides();
+        slides.forEach(slide => {
+            if (slide.getAttribute('data-slider') == dot.getAttribute('data-toggle')) {
+                slide.classList.add('slide-active');
+            }
+        });
+    });
 });
